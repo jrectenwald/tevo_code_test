5762366cc85675f1e75b63463d87235ad3739272
jrectenwald <jimrec101@gmail.com>
2016-02-10 21:32:44 -0500

    add run tests

---
+++ run_tests.rb
@@ -0,0 +1,20 @@
+require_relative 'list_sorter'
+
+sorter = ListSorter.new
+
+puts "Output 1:"
+puts sorter.output_one
+
+
+puts "\n\n\n"
+
+
+puts "Output 2:"
+puts sorter.output_two
+
+
+puts "\n\n\n"
+
+
+puts "Output 3:"
+puts sorter.output_three
